dc16f8956bcf97d9ee946911d44faffedaecdc4f
Lilou Sicard-Noel <lilousn@gmail.com>
2021-10-26 11:57:59 -0700

    HardCoding Version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+
+    @FXML Label signIN;
+
+    public void print(){
+        System.out.println("Click");
+    }
+
+    public void print2(){
+        System.out.println("Click");
+    }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,8 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 
@@ -14,9 +20,33 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("UserInterface.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("UserInterface.fxml"));
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: #c87a4f");
+        root.setPadding(new Insets(20,20,20,20));
         primaryStage.setTitle("Learning Java With Java");
-        Scene scene = new Scene(root, 600, 400,Color.rgb(205,135,101));
+
+        Button signIn = new Button("Sign In");
+        signIn.setStyle("-fx-background-color: #cd8765");
+        signIn.setMinSize(300,10);
+        signIn.setPrefHeight(55);
+        signIn.setFont(new Font("Aria",30));
+
+        Button signUp = new Button("Sign Up");
+        signUp.setStyle("-fx-background-color: #cd8765");
+        signUp.setMinSize(300,10);
+        signUp.setPrefHeight(55);
+        signUp.setFont(new Font("Aria",30));
+
+
+        VBox vbButton = new VBox();
+        vbButton.setSpacing(10);
+        vbButton.setPadding(new Insets(0,20,10,20));
+        vbButton.getChildren().addAll(signUp,signIn);
+        vbButton.setAlignment(Pos.BOTTOM_CENTER);
+        root.getChildren().addAll(vbButton);
+
+        Scene scene = new Scene(root, 800, 400);
         primaryStage.setScene(scene);
 
 
